9f062f923edcd4d9e86249d854c5e9137ffb06a0
Sharanidhi <108647751+Sharanidhi@users.noreply.github.com>
2023-06-07 14:43:28 +0530

    Update diffie-hellman.py

--- diffie-hellman.py
+++ diffie-hellman.py
@@ -1,48 +1,60 @@
-import string
+# Diffie-Hellman Code
 
-def affine_encrypt(plain_text, a, b):
-    alphabet = string.ascii_uppercase
-    encrypted_text = ""
-    
-    for char in plain_text:
-        if char in alphabet:
-            index = alphabet.index(char)
-            encrypted_index = (a * index + b) % 26
-            encrypted_char = alphabet[encrypted_index]
-            encrypted_text += encrypted_char
-        else:
-            encrypted_text += char
-    
-    return encrypted_text
+def prime_checker(p):
+	# Checks If the number entered is a Prime Number or not
+	if p < 1:
+		return -1
+	elif p > 1:
+		if p == 2:
+			return 1
+		for i in range(2, p):
+			if p % i == 0:
+				return -1
+			return 1
 
-def affine_decrypt(cipher_text, a, b):
-    alphabet = string.ascii_uppercase
-    decrypted_text = ""
-    a_inverse = 0
-    
-    for i in range(26):
-        if (a * i) % 26 == 1:
-            a_inverse = i
-            break
-    
-    for char in cipher_text:
-        if char in alphabet:
-            index = alphabet.index(char)
-            decrypted_index = (a_inverse * (index - b)) % 26
-            decrypted_char = alphabet[decrypted_index]
-            decrypted_text += decrypted_char
-        else:
-            decrypted_text += char
-    
-    return decrypted_text
+def primitive_check(g, p, L):
+	# Checks If The Entered Number Is A Primitive Root Or Not
+	for i in range(1, p):
+		L.append(pow(g, i) % p)
+	for i in range(1, p):
+		if L.count(i) > 1:
+			L.clear()
+			return -1
+		return 1
 
-# Example usage:
-plain_text = input("Enter the plain text: ")
-a = int(input("Enter the multiplicative value: "))
-b = int(input("Enter the additive value: "))
+l = []
+while 1:
+	P = int(input("Enter P : "))
+	if prime_checker(P) == -1:
+		print("Number Is Not Prime, Please Enter Again!")
+		continue
+	break
 
-encrypted_text = affine_encrypt(plain_text, a, b)
-print("Encrypted text:", encrypted_text)
+while 1:
+	G = int(input(f"Enter The Primitive Root Of {P} : "))
+	if primitive_check(G, P, l) == -1:
+		print(f"Number Is Not A Primitive Root Of {P}, Please Try Again!")
+		continue
+	break
 
-decrypted_text = affine_decrypt(encrypted_text, a, b)
-print("Decrypted text:", decrypted_text)
+# Private Keys
+x1, x2 = int(input("Enter The Private Key Of User 1 : ")), int(
+	input("Enter The Private Key Of User 2 : "))
+while 1:
+	if x1 >= P or x2 >= P:
+		print(f"Private Key Of Both The Users Should Be Less Than {P}!")
+		continue
+	break
+
+# Calculate Public Keys
+y1, y2 = pow(G, x1) % P, pow(G, x2) % P
+
+# Generate Secret Keys
+k1, k2 = pow(y2, x1) % P, pow(y1, x2) % P
+
+print(f"\nSecret Key For User 1 Is {k1}\nSecret Key For User 2 Is {k2}\n")
+
+if k1 == k2:
+	print("Keys Have Been Exchanged Successfully")
+else:
+	print("Keys Have Not Been Exchanged Successfully")
