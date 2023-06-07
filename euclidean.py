afb0bbf240cb323e9bd6de1541d92dd51d959773
Sharanidhi <108647751+Sharanidhi@users.noreply.github.com>
2023-06-07 14:39:40 +0530

    Update euclidean.py

--- euclidean.py
+++ euclidean.py
@@ -1,48 +1,23 @@
-import string
+def extended_euclidean_algorithm(a, b):
+    if b == 0:
+        return a, 1, 0
 
-def affine_encrypt(plain_text, a, b):
-    alphabet = string.ascii_uppercase
-    encrypted_text = ""
-    
-    for char in plain_text:
-        if char in alphabet:
-            index = alphabet.index(char)
-            encrypted_index = (a * index + b) % 26
-            encrypted_char = alphabet[encrypted_index]
-            encrypted_text += encrypted_char
-        else:
-            encrypted_text += char
-    
-    return encrypted_text
+    gcd, x1, y1 = extended_euclidean_algorithm(b, a % b)
+    x = y1
+    y = x1 - (a // b) * y1
 
-def affine_decrypt(cipher_text, a, b):
-    alphabet = string.ascii_uppercase
-    decrypted_text = ""
-    a_inverse = 0
-    
-    for i in range(26):
-        if (a * i) % 26 == 1:
-            a_inverse = i
-            break
-    
-    for char in cipher_text:
-        if char in alphabet:
-            index = alphabet.index(char)
-            decrypted_index = (a_inverse * (index - b)) % 26
-            decrypted_char = alphabet[decrypted_index]
-            decrypted_text += decrypted_char
-        else:
-            decrypted_text += char
-    
-    return decrypted_text
+    return gcd, x, y
+
+def find_modular_inverse(a, m):
+    gcd, x, y = extended_euclidean_algorithm(a, m)
+    if gcd != 1:
+        raise ValueError("Inverse does not exist.")
+
+    return x % m
 
 # Example usage:
-plain_text = input("Enter the plain text: ")
-a = int(input("Enter the multiplicative value: "))
-b = int(input("Enter the additive value: "))
+a = int(input("Enter a number to find inverse: "))
+m = int(input("Enter the number whose modulus is to be found: "))
 
-encrypted_text = affine_encrypt(plain_text, a, b)
-print("Encrypted text:", encrypted_text)
-
-decrypted_text = affine_decrypt(encrypted_text, a, b)
-print("Decrypted text:", decrypted_text)
+inverse = find_modular_inverse(a, m)
+print("Modular inverse of", a, "mod", m, "is:", inverse)
